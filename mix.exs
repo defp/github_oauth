80d05dabcb12f17a45ef39e2cecfeccdb65ff10d
lidashuang <lidashuang@3pjgames.com>
2015-04-07 16:48:58 +0800

    add deps

--- mix.exs
+++ mix.exs
@@ -25,6 +25,7 @@ defmodule GithubOauth.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [{:httpoison, "~> 0.6"},
+    {:poison, "~> 1.3"}]
   end
 end
